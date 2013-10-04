05a97654e9006f50a170a00ca03bb18598c49f9a
ElenaGrekis <elena_belladonna@mail.ru>
2013-10-05 01:47:55 +0400

    Added year lab

---
+++ practice_2/leapyear.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+def isLeapYear( y ):
+    if(not(y % 400) or ((y % 100) and not(y & 3))):
+        return True
+    else:
+        return False
+
+try:
+    year = int(raw_input('Enter year: '))
+    if(year > 0 and year < 9999):
+        if(isLeapYear(year)):
+            print "Cool the year is leap!"
+        else:
+            print ":( not leap year"
+except (ValueError, TypeError):
+    print "Error in year! Try again"
