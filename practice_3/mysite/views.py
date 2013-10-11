7b6458d10352e0a6c1add2af24d8823671bedf7e
Elena Grekis <elena_belladonna@mail.ru>
2013-10-12 02:52:18 +0400

    Задание 3. Табличное представление данных.

--- practice_3/mysite/views.py
+++ practice_3/mysite/views.py
@@ -5,15 +5,16 @@ from django.shortcuts import render_to_response
 def listing(request, path):
     base = "C:/" #/var
 
+    print base + path
     allfiles = os.listdir(base + path)
     files = []
     dirs = []
     for i in allfiles:
         fullpath = base + path + "/" + i
+        print fullpath
         if(os.path.isfile(fullpath)):
-            files.append(i)
+            files.append( { "name": i, "size": os.path.getsize(fullpath), "mtime": time.ctime(os.path.getmtime(fullpath))  } )
         elif(os.path.isdir(fullpath)):
-            dirs.append(i)
+            dirs.append( { "name": i, "size": os.path.getsize(fullpath), "mtime": time.ctime(os.path.getmtime(fullpath))  } )
       
-
     return render_to_response('listing.html', {'directory': base + path, 'file_list': files, 'dir_list': dirs})
