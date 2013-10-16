6163f4d686e590ed7075caafc78ed14b9957be01
Elena Grekis <elena_belladonna@mail.ru>
2013-10-16 17:36:18 +0400

    Задание 2. Вывод карточки книги. (+вывод через строковое представление модели).
    Исправления в задании 1.

--- practice_4/library/models.py
+++ practice_4/library/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from django.core.urlresolvers import reverse
 
 class Author(models.Model):
     first_name = models.CharField(max_length=32)
@@ -7,7 +8,9 @@ class Author(models.Model):
     email = models.EmailField(null=True)
 
     def __unicode__(self):
-        return self.first_name + self.last_name
+        return u'%s %s' % (self.first_name, self.last_name)
+    def get_absolute_url(self):      
+        return reverse('show_author',  args=[str(self.id)] )
 
 class Publisher(models.Model):
     title = models.CharField(max_length=32)
@@ -17,7 +20,7 @@ class Publisher(models.Model):
     website = models.URLField()
 
     def __unicode__(self):
-        return self.title + "(" + self.website + ")"
+        return u'%s (%s, %s)' % (self.title, self.city, self.country)
 
 
 class Book(models.Model):
@@ -28,7 +31,7 @@ class Book(models.Model):
 
     def __unicode__(self):
         return self.title
-    def get_absolute_url(self):
-        from django.core.urlresolvers import reverse
-        return reverse('show',  args=[str(self.id)] )
+    
+    def get_absolute_url(self):      
+        return reverse('show_book',  args=[str(self.id)] )
     
--- practice_4/library/views.py
+++ practice_4/library/views.py
@@ -13,10 +13,18 @@ def index(request):
     return HttpResponse(template.render(context))
 
 
-def show(request, book): #FIXED in task2
-    books = Book.objects.all()
-    template = loader.get_template('index.html')
+def show_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    template = loader.get_template('book.html')
     context = RequestContext(request, {
-        'books': books,
+        'book': book,
+    })
+    return HttpResponse(template.render(context))
+
+def show_author(request, author_id): #FIXED IN Task3
+    book = Book.objects.get(id=author_id)
+    template = loader.get_template('book.html')
+    context = RequestContext(request, {
+        'book': book,
     })
     return HttpResponse(template.render(context))
