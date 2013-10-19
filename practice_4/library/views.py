dc0fb8ee2c3cd5a3f73049470ff19edb6d981a58
Elena Grekis <elena_belladonna@mail.ru>
2013-10-19 13:33:30 +0400

    Исправлено в соответствии с замечанием

--- practice_4/library/views.py
+++ practice_4/library/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import RequestContext, loader
 
 from library.models import Book
@@ -7,35 +7,31 @@ from library.models import Author
 
 def index(request):
     books = Book.objects.all()
-    template = loader.get_template('index.html')
     context = RequestContext(request, {
         'books': books,
     })
-    return HttpResponse(template.render(context))
+    return render(request, 'index.html', context)
 
 
 def show_book(request, book_id):
     book = Book.objects.get(id=book_id)
-    template = loader.get_template('book.html')
     context = RequestContext(request, {
         'book': book,
     })
-    return HttpResponse(template.render(context))
+    return render(request, 'book.html', context)
 
 def show_author(request, author_id):
     author = Author.objects.get(id=author_id)
-    template = loader.get_template('author.html')
     context = RequestContext(request, {
         'author': author,
     })
-    return HttpResponse(template.render(context))
+    return render(request, 'author.html', context)
 
 
 def list_authors(request):
     authors = Author.objects.all()
-    template = loader.get_template('list_authors.html')
     context = RequestContext(request, {
         'authors': authors,
     })
-    return HttpResponse(template.render(context))
+    return render(request, 'list_authors.html', context)
 
