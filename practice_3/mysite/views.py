a4c207f5c900ff3f23acd1e4808eee345fd33482
Elena Grekis <elena_belladonna@mail.ru>
2013-10-19 13:38:52 +0400

    Исправлено в соответствии с замечанием

--- practice_3/mysite/views.py
+++ practice_3/mysite/views.py
@@ -10,10 +10,9 @@ def listing(request, path):
     dirs = []
     for i in allfiles:
         fullpath = base + path + "/" + i
-        print fullpath
         if(os.path.isfile(fullpath)):
             files.append( { "name": i, "size": os.path.getsize(fullpath), "mtime": time.ctime(os.path.getmtime(fullpath))  } )
-        elif(os.path.isdir(fullpath)):
+        else
             dirs.append( { "name": i, "size": os.path.getsize(fullpath), "mtime": time.ctime(os.path.getmtime(fullpath))  } )
       
     return render_to_response('listing.html', {'directory': base + path, 'file_list': files, 'dir_list': dirs, 'path': path })
