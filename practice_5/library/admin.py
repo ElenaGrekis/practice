298d318fdf6c1c7f60cc4e19ddfc3a778b9117b1
Elena Grekis <elena_belladonna@mail.ru>
2013-10-25 18:18:09 +0400

    Хоумворк 5. Админка

---
+++ practice_5/library/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.contrib.contenttypes import generic
+from library.models import Book, BooksImage, Publisher
+from django.contrib.contenttypes.models import ContentType
+
+class ImageInline(generic.GenericTabularInline):
+    model = BooksImage
+    extra = 0
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = [ 'title', 'publisher', 'publication_date', 'books' ]
+    fields = [ 'title', 'authors', 'publisher'  ]
+
+    def books(self, obj):
+        return BooksImage.objects.filter(book_id=obj.id).count()
+
+    inlines = [
+        ImageInline,
+    ]
+
+
+admin.site.register(Book, BookAdmin)
--- practice_5/library/models.py
+++ practice_5/library/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from datetime import datetime
 from django.core.urlresolvers import reverse
-
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
 
 class Author(models.Model):
     first_name = models.CharField(max_length=32)
@@ -38,12 +39,14 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse('show_book', args=[str(self.id)])
 
+
 class BooksImage(models.Model):
     small = models.ImageField(upload_to="s_images")
     big = models.ImageField(upload_to="b_images", null=True, blank=True)
-
-    def __init__(self):
-        self.__cover = "No cover"
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey("content_type", "object_id")
+    book = models.ForeignKey(Book)
 
     def getcover(self):
         return self.__cover
