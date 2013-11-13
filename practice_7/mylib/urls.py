0666922880e14be7bfc1c288147e31e856b1d45b
Elena Grekis <elena_belladonna@mail.ru>
2013-11-13 21:59:52 +0300

    Пропущенный файл

--- practice_7/mylib/urls.py
+++ practice_7/mylib/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import patterns, include, url
 
 from library.views import BookListView, AuthorListView, BookDetailView, AuthorDetailView
 
+from django.contrib.auth.views import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from library.views import register
+
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -18,6 +22,9 @@ urlpatterns = patterns(
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', login),
+    url(r'^logout/$', logout),
+    url(r'^register/$', register),
     url(r'^library/$', BookListView.as_view()),
     url(r'^library/books/$', BookListView.as_view()),
     url(r'^library/books/(?P<pk>\d+)/$', BookDetailView.as_view(), name='show_book'),
